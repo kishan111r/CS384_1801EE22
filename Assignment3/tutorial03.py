393176854ba39a67c72fdd444d0950fe4dddd79b
kishan111r <kishan111r@gmail.com>
2020-10-14 22:36:34 +0530

    Completed the funtion course and created all the files and csv files as demanded

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -2,18 +2,19 @@ import csv
 import os
 import string
 os.system("clear")
-header = ['id','full_name','country','email','gender','dob','blood_group','state']
+# header = ['id','full_name','country','email','gender','dob','blood_group','state']
 
-def writeFile(file_name,item):
+def writeFile(file_name,item,header):
     flag=0
     if os.path.isfile(file_name):
         flag=1
     
-    with open(file_name,'w') as file:
+    with open(file_name,mode= '+a') as file:
         writer = csv.writer(file)
         if(flag==0):
             writer.writerow(header)
         writer.writerow(item)
+    file.close()
 
 
 
@@ -21,30 +22,40 @@ def course():
     # Read csv and process
     #print(os.getcwd())
     with open("studentinfo_cs384.csv",'r') as file:
-        my_file = csv.reader(file,delimiter=',',skipinitialspace=True)
+        my_file = csv.DictReader(file,delimiter=',',skipinitialspace=True)
         next(my_file)
         for row in my_file:
-            roll_no = row[0]
-            year = roll_no[0:2]
-            stream_code = roll_no[2:4]
-            branch = roll_no[4:6]
-            serial = roll_no[6:8]
-            file_name = "misc.csv"
-            if year.isdigit() and stream_code.isdigit() and branch.isalpha() and serial.isdigit() and len(roll_no)==8:
-                # path_misc= os.path.join(os.getcwd(),"analytics/course")
-                # os.chdir(path_misc)
-                # writeFile(file_name,row)
+            data = list(row.values())
+            header = list(row.keys())
+            roll_no = row['id']
+            year = str(roll_no[0:2])
+            stream_code = str(roll_no[2:4])
+            branch = str(roll_no[4:6])
+            serial = str(roll_no[6:8])
+            #file_name = "misc.csv"
+
+            general_path = os.getcwd()
+            analytics_path = os.path.join(general_path,'analytics')
+            if(os.path.exists(analytics_path)==False):
+                os.mkdir(analytics_path)
+            course_path= os.path.join(analytics_path,'course')
+            if(os.path.exists(course_path)==False):
+                os.mkdir(course_path)
+            
+            if not (year.isdigit() and stream_code.isdigit() and branch.isalpha() and serial.isdigit() and len(roll_no)==8):
+                misc_path = os.path.join(course_path,'misc.csv')
+                writeFile(misc_path,data,header)
                 continue
+                # if(n>30):
+                #     break
 
             branch=branch.lower()
-            general_path = os.getcwd()
-            branch_path = os.path.join(general_path,"analytics/course")
-            # branch_path = general_path+"/analytics/course"
-            #print(branch_path)
-            branch_path = os.path.join(branch_path,branch)
+            branch_path = os.path.join(course_path,branch)
             if(os.path.exists(branch_path)==False):
                 os.mkdir(branch_path)
-            stream=''
+            # print(stream_code)
+            # break
+            #stream=''
             if(stream_code=='01'):
                 stream = 'btech'
             elif(stream_code=='11'):
@@ -56,10 +67,13 @@ def course():
             stream_path =os.path.join(branch_path,stream)
             if(os.path.exists(stream_path)==False):
                 os.mkdir(stream_path)
-            os.chdir(stream_path)
             file_name= year+'_'+branch+'_'+stream+".csv"
-            writeFile(file_name,row)
-            os.chdir("../../../..")
+            file_path=os.path.join(stream_path,file_name)
+            writeFile(file_path,data,header)
+            
+    file.close()
+    
+
 
 
             
