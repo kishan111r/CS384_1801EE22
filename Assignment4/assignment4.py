6c1a50176002ec53d2e117a9bcbb658ba93a91ec
kishan111r <kishan111r@gmail.com>
2020-11-16 00:11:50 +0530

    Completed the result calculation function with calculation of CPI and SPI and storing it in overall file individually

--- Assignment4/assignment4.py
+++ Assignment4/assignment4.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 current_path = os.getcwd()
 delete_path = os.path.join(current_path, 'grades')
-
+max_sem_no={}
 # Function to delete the folders if it exist do that replica is not made and we can everytime make a new folder.
 def del_grades_folder():
     if os.path.isdir(delete_path):
@@ -22,6 +22,7 @@ def roll_number_individual_result():
             data_reader = csv.DictReader(data_file)
             for row in data_reader:
                 roll_no = row['roll']
+                max_sem_no[roll_no] = row['sem']
                 current_path=os.getcwd()
                 folder_path = os.path.join(current_path, 'grades')
                 file_name = roll_no + '_individual.csv'
@@ -82,5 +83,155 @@ def roll_number_individual_result():
     except:
         print("Error while opening acad_res_stud_grades.csv")
 
+# Since we have already compile the students individual record now we can calculate the Overall performance of the student.
+
+
+def roll_number_overallresult():
+    folder_path = os.path.join(current_path, 'grades')
+    grades = {'AA':10, 'AB':9, 'BB':8, 'BC':7, 'CC':6, 'CD':5, 'DD':4, 'F':0, 'I':0} # Valid Grades of the Students in a Semester Grades
+    os.chdir(folder_path)
+    for ind_file in os.listdir(folder_path):
+        if str(ind_file) != 'misc.csv':
+            file_n = re.split('_', str(ind_file), maxsplit=1)
+            roll_no = file_n[0]
+            if(file_n[1] == 'overall.csv'):
+                continue
+            else:
+                data_frame = pd.read_csv(ind_file, skiprows=2) #Used Pandas for modifing and working for result generation.
+                data = dict(data_frame)
+                Gr = list(data['Grade'])
+                i = 0
+                backlog = []
+                for grade in Gr:
+                    if grade == 'F':
+                        backlog.append(i-1)
+                    elif grade == 'I':
+                        backlog.append(i-1)
+                    i += 1
+                sem_no = set(data_frame['Sem'])
+                #print(backlog)
+                keys = list(sem_no)
+                sem = {}
+                semwise_score = {}
+                semwise_total_credits = {}
+                semwise_credit_cleared = {}
+                total_credits = {}
+                total_credits_cleared = {}
+                SPI = {}
+                CPI = {}
+                for key in keys:
+                    try:
+                        sem[key] = key
+                        semwise_score[key] = 0
+                        semwise_total_credits[key] = 0
+                        semwise_credit_cleared[key] = 0
+                        SPI[key] = 0
+                        CPI[key] = 0
+                        total_credits[key] = 0
+                        total_credits_cleared[key] = 0
+                    except:
+                        print(f"There is some error roll no :{row['roll']}")
+                i = 0
+                for value in data_frame['Sem']:
+                    try:
+                        # if(data_frame['Grade'][i]=='F'):
+                        #     backlog.append
+                        semwise_score[value] += grades[data_frame['Grade'][i]]*data_frame['Credits'][i]
+                        if data_frame['Grade'][i] != 'F' and data_frame['Grade'][i] != 'I':
+                            semwise_credit_cleared[value] += data_frame['Credits'][i]
+                        semwise_total_credits[value] += data_frame['Credits'][i]
+
+                    except:
+                        print(f"There is some error roll no :{row['roll']}")
+                    i += 1
+                for key in keys:
+                    try:
+                        if semwise_credit_cleared[key] != 0:
+                            SPI[key] = round(semwise_score[key] /
+                                             semwise_total_credits[key], 2)
+                        else:
+                            SPI[key] = 0
+                    except:
+                        print(f"There is some error")
+                i = 0
+                for key in keys:
+                    try:
+                        if i == 0 and i not in backlog:
+                            total_credits[key] = semwise_total_credits[key]
+                            total_credits_cleared[key] = semwise_total_credits[key]
+                            x = total_credits[key]
+                            y = total_credits_cleared[key]
+                        elif i == 0 and i in backlog:
+                            total_credits[key] = semwise_total_credits[key]
+                            total_credits_cleared[key] = 0
+                            x = total_credits[key]
+                            y = total_credits_cleared[key]
+                        elif i not in backlog:
+                            total_credits[key] += (semwise_total_credits[key] + x)
+                            total_credits_cleared[key] += (
+                                semwise_total_credits[key] + y)
+                            x = total_credits[key]
+                            y = total_credits_cleared[key]
+                        elif i in backlog:
+                            total_credits[key] += (semwise_total_credits[key] + x)
+                            x = total_credits[key]
+                            total_credits_cleared[key] = y
+                    except:
+                        print(f"There is some error for roll no :{row['roll']}")
+                    i += 1
+                i = 0
+                for key in keys:
+                    try:
+                        x = 0
+                        y = 0
+                        for j in sem:
+                            if int(j)<=int (key):
+                                x += SPI[j]*semwise_total_credits[j]
+                        if total_credits[key] != 0:
+                            y = x/total_credits[key]
+                        else:
+                            y = 0
+                        CPI[key] = round(y, 2)
+                        i += 1
+                    except:
+                        print(f'Error occured while working on -> {ind_file}')
+                data_entry = []
+                for key in range(1,int(max_sem_no[roll_no])+1):
+                    if(key in keys):
+                        l = [sem[key], semwise_total_credits[key], semwise_credit_cleared[key], SPI[key], total_credits[key], total_credits_cleared[key], CPI[key]]
+                    else:
+                        l=[key,0,0,0,0,0,0]
+                    data_entry.append(l)
+                file_name = roll_no + '_overall.csv'
+                file_path = os.path.join(folder_path, file_name)
+                for row in data_entry:
+                    if os.path.isfile(file_path):
+                        # opening file in append mode, then writing the current row, with header
+                        try:
+                            with open(file_path, 'a+', newline='') as write_file:
+                                writer = csv.writer(write_file)
+                                writer.writerow(row)
+                                write_file.close()
+                        except:
+                            print(f"Error opening {file_name}")
+                    else:
+                        try:
+                            with open(file_path, 'a+', newline='') as write_file:
+                                write_file.write(f'Roll: {roll_no}\n')
+                                writer = csv.writer(write_file)
+                                fieldnames = ['Semester', 'Semester Credits', 'Semester Credits Cleared', 'SPI', 'Total Credits', 'Total Credits Cleared', 'CPI']
+                                writer.writerow(fieldnames)
+                                writer.writerow(row)
+                                write_file.close()
+                        except:
+                            print(f"There is some error opening the File:- {file_name} to write roll no :{row['roll']}")
+
+
+
+
+
+
+
 del_grades_folder()
 roll_number_individual_result()
+roll_number_overallresult()
