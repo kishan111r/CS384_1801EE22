c09e4a6f0a2259bce24445f6b8502fe78c356711
kishan111r <kishan111r@gmail.com>
2020-11-30 06:02:55 +0530

    Copied the folder of Ensem code from the skeleton folder

---
+++ End_Sem_Code/end_sem_group_allocation.py
@@ -0,0 +1,22 @@
+import os
+import csv
+import math
+import pandas as pd
+
+
+def group_allocation(filename, number_of_groups):
+    # Entire Logic 
+	# You can add more functions, but in the test case, we will only call the group_allocation() method,
+    curr_dir =  os.getcwd()
+    os.path.join(curr_dir,filename)
+    
+
+
+
+
+
+
+
+filename = "Btech_2020_master_data.csv"
+number_of_groups = 12 
+group_allocation(filename, number_of_groups)
